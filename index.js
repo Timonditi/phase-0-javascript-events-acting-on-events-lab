47cfcb8f4b13dcfc857fc219f8f2febd2a6bd32c
Timothy <todongoonditi@gmail.com>
2023-03-16 11:48:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left, 10);
+    
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left, 10);
+    
+    if (left < (400 - dodger.offsetWidth)) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  
